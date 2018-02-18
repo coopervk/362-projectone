dfce39116b1217a6a094c935d291eeb3d3775817
coopervk <coopervk@gmail.com>
2018-02-18 16:47:21 -0600

    Linting part 1

--- site.js
+++ site.js
@@ -5,45 +5,45 @@ $('#contact-form').on('submit',
     var username = $('#username').val();
     var password = $('#password').val();
     // Declare regexes for form elements
-    var emailcheck_regex = /.+@.+\..+/g;
-    var usernamecheck_regex = /[\w]+/g;
-    var passwordcheck_regex = /.+/g;
+    var emailcheckregex = /.+@.+\..+/g;
+    var usernamecheckregex = /[\w]+/g;
+    var passwordcheckregex = /.+/g;
     // Check the elements against their regexes
-    var email_results = emailcheck_regex.exec(email);
-    var username_results = usernamecheck_regex.exec(username);
-    var password_results = passwordcheck_regex.exec(password);
+    var emailresults = emailcheckregex.exec(email);
+    var usernameresults = usernamecheckregex.exec(username);
+    var passwordresults = passwordcheckregex.exec(password);
     // Prevent standar behavior of submission
     e.preventDefault();
     // Create element to talk to user about form data if it does not exist
-    if($("#form_check_results").length === 0) {
-      $("<p id='form_check_results' class=''>TEST</p>").insertAfter($(this));
+    if($("#form-check-results").length === 0) {
+      $("<p id='form-check-results' class=''>TEST</p>").insertAfter($(this));
     }
     // Reset each form element's class, removing markings on now possibly correct elements
     $('#email').attr('class', "");
     $('#username').attr('class', "");
     $('#password').attr('class', "");
     // Yell at user if their email is invalid
-    if(!email_results || email_results.length > 1) {
-      $("#form_check_results").attr('class', 'error_message');
-      $("#form_check_results").text("Uh oh! Your email appears incorrect! Try again.");
-      $('#email').attr('class', 'incorrect_input');
+    if(!emailresults || emailresults.length > 1) {
+      $("#form-check-results").attr('class', 'error-message');
+      $("#form-check-results").text("Uh oh! Your email appears incorrect! Try again.");
+      $('#email').attr('class', 'incorrect-input');
     }
     // Yell at user if their username is invalid
-    else if(!username_results || username_results.length > 1) {
-      $("#form_check_results").attr('class', 'error_message');
-      $("#form_check_results").text("Your username is invalid!");
-      $('#username').attr('class', 'incorrect_input');
+    else if(!usernameresults || usernameresults.length > 1) {
+      $("#form-check-results").attr('class', 'error-message');
+      $("#form-check-results").text("Your username is invalid!");
+      $('#username').attr('class', 'incorrect-input');
     }
     // Yell at user if their password is invalid
-    else if(!password_results || password_results.length > 1) {
-      $("#form_check_results").attr('class', 'error_message');
-      $("#form_check_results").text("You need to enter a password!");
-      $('#password').attr('class', 'incorrect_input');
+    else if(!passwordresults || passwordresults.length > 1) {
+      $("#form-check-results").attr('class', 'error-message');
+      $("#form-check-results").text("You need to enter a password!");
+      $('#password').attr('class', 'incorrect-input');
     }
     // Thank user for completing form
     else {
-      $("#form_check_results").attr('class', 'thanks_message');
-      $("#form_check_results").text("Thank you for signing up, " + username_results[0] + "!");
+      $("#form-check-results").attr('class', 'thanks-message');
+      $("#form-check-results").text("Thank you for signing up, " + usernameresults[0] + "!");
       $(this).remove();
     }
   }
