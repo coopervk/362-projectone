845ef2700fb2af5cea8303cb53ea78cc86d99f73
coopervk <coopervk@gmail.com>
2018-02-18 14:46:44 -0600

    Add background-color changing functionality to element testing

--- site.js
+++ site.js
@@ -18,20 +18,27 @@ $('#contact-form').on('submit',
     if($("#form_check_results").length === 0) {
       $("<h1 id='form_check_results' class=''>TEST</h1>").insertAfter($(this));
     }
+    // Reset each form element's class, removing markings on now possibly correct elements
+    $('#email').attr('class', "");
+    $('#username').attr('class', "");
+    $('#password').attr('class', "");
     // Yell at user if their email is invalid
     if(!email_results || email_results.length > 1) {
       $("#form_check_results").attr('class', 'error_message');
       $("#form_check_results").text("Uh oh! Your email appears incorrect! Try again.");
+      $('#email').attr('class', 'incorrect_input');
     }
     // Yell at user if their username is invalid
     else if(!username_results || username_results.length > 1) {
       $("#form_check_results").attr('class', 'error_message');
-      $("#form_check_results").text("Your username is invalid! No special characters.");
+      $("#form_check_results").text("Your username is invalid!");
+      $('#username').attr('class', 'incorrect_input');
     }
     // Yell at user if their password is invalid
     else if(!password_results || password_results.length > 1) {
       $("#form_check_results").attr('class', 'error_message');
       $("#form_check_results").text("You need to enter a password!");
+      $('#password').attr('class', 'incorrect_input');
     }
     // Thank user for completing form
     else {
